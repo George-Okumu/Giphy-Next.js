ab3b8637407341998e3b3fb5e2858e07873d6715
George Okumu <gokumu12@gmail.com>
2021-05-12 17:39:50 +0300

    Link from home page to search page done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,8 +11,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section>
-        <h1>This is my giphy application meant for SEO(Search Engine Optimization)</h1>
+        <h1>Picky Picky Lovers of Giphys.</h1>
+        <p>
+          Love Giphys? <b>So do we</b>, Use{" "}
+          <a href="https://github.com/George-Okumu/Giphy-Next.js">
+            Giphy-Next.js Geo-Project
+          </a>
+          to learn how to intergrate Giphys APi and be proud.
+        </p>
       </section>
+      
+      <section>
+        <Link href="/search">
+        <a>Search Giphy by classification Name</a>
+        </Link>
+      </section>
+      <footer>
+        Created by George Okumu
+        <a href="https://github.com/George-Okumu/Giphy-Next.js">Source Code</a>
+      </footer>
     </div>
-  )
+  );
 }
---
+++ pages/search.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+
+export default function Search(){
+    return(
+        <>
+        <Head>
+            <title>Search Giphy</title>
+        </Head> 
+        <main>
+            <p>This is where the SEO resides</p>
+        </main>
+        </>
+    )
+}
